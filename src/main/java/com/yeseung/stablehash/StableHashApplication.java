9d519ee5107c6739e5831c3395fbd85981684252
이승구 <leesg107@naver.com>
2024-01-02 11:32:15 +0900

    feat: first commit

---
+++ src/main/java/com/yeseung/stablehash/StableHashApplication.java
@@ -0,0 +1,13 @@
+package com.yeseung.stablehash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StableHashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StableHashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yeseung/stablehash/StableHashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeseung.stablehash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StableHashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
