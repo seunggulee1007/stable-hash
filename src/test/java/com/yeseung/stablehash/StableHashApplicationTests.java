714dee9ef8408e75110ea31630d60d5f7bb49be2
이승구 <leesg107@naver.com>
2024-06-02 15:22:13 +0900

    docs: readme파일추가

--- src/test/java/com/yeseung/stablehash/StableHashApplicationTests.java
+++ src/test/java/com/yeseung/stablehash/StableHashApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StableHashApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
